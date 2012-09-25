a72b0504e1a3dc4869c4cd7e4292405e3e3ae336
Vineet Naik <vineet.naik@kodeplay.com>
2012-09-25 13:19:15 +0530

    Fix insufficient funds case in atm solution

--- atm/atm.erl
+++ atm/atm.erl
@@ -14,7 +14,7 @@ withdraw(Amount, Initial) ->
 balance(Amount, Initial) ->
     balance(Amount, Initial, 0.50).
 
-balance(Amount, Initial, _) when Initial < Amount -> 
+balance(Amount, Initial, Charges) when Initial < (Amount + Charges) -> 
     {failed, Initial, "In sufficient funds"};
 balance(Amount, Initial, _) when Amount rem 5 > 0 -> 
     {failed, Initial, "Amount not multiple of 5"};
@@ -27,6 +27,7 @@ atm_test() ->
     {ok, 0.00} = balance(5, 5.50),
     {failed, 150.0, _} = balance(12, 150.00),
     {failed, 150.0, _} = balance(200, 150.00),
+    {failed, 5.00, _} = balance(5, 5.00),
     ok.
 
     
